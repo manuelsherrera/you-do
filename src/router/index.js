99471b039994d5b57ecb8d488732141f7ca8ffbd
manuelsherrera <manuelsherrera@gmail.com>
2022-12-12 12:29:26 +0100

    dashboard creado, router-view

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Dasboard from "../views/DashboardViews.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      component: Dasboard,
+    },
+  ],
 });
 
 export default router;
