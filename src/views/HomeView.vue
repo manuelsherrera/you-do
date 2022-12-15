63efbc2454626afead1431ede1c39124084ca183
0xNordian <85548493+0xNordian@users.noreply.github.com>
2022-12-15 22:33:23 +0100

    Hero, Pricing and Team sections completed

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+    <HERO />
+    <PRICING />
+    <TEAM />
+
+</template>
+
+<script>
+import HERO from "../components/Hero.vue"
+import PRICING from "../components/Pricing.vue"
+import TEAM from "../components/Team.vue"
+
+export default {
+    components: {
+        HERO,
+        PRICING,
+        TEAM,
+    }
+}
+</script>
+
+<style>
+
+</style>
