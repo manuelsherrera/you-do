4db160e7572b134868b6f4026fd7783959fa0500
0xNordian <85548493+0xNordian@users.noreply.github.com>
2022-12-21 18:36:12 +0100

    Fixes on css blobs on the home and login pages

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,7 +4,7 @@
             <div class="item-end w-1/4 p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800"
                 role="alert">
                 <span class="font-bold">We have sent you a confirmation email!</span> Please, confirm your email!.
-            </div>
+            </div> 
         </div>
         <div class="px-6 py-12 lg:py-24 md:px-12 text-center lg:text-left">
             <div class="container mx-auto xl:px-32 text-gray-800">
