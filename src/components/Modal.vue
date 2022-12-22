2bd21d004aade56ca889f17c90948d35f47391aa
manuelsherrera <manuelsherrera@gmail.com>
2022-12-22 11:19:26 +0100

    add: succesful task created / modal.vue

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -49,6 +49,23 @@
             </button>
         </div>
     </div>
+    <!-- <div ></div> -->
+    <!--  <div v-if="succesfullTask" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+        role="alert">{{ taskCreated }}
+        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20">
+                <title>Close</title>
+                <path
+                    d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+        </span>
+    </div> -->
+    <div v-if="succesfullTask" class="flex justify-center bg-blue-100 border-blue-500 text-blue-700 px-4 py-3"
+        role="alert">
+        <p class="font-bold">{{ taskCreated }}</p>
+
+    </div>
 </template>
   
 <script>
@@ -67,7 +84,8 @@ export default {
             cols: 50,
             taskTitle: "",
             taskDescription: "",
-            succesfull: "Task succesfully created",
+            succesfullTask: false,
+            taskCreated: "New task created!!",
         };
     },
     components: {
@@ -85,6 +103,9 @@ export default {
             this.tasksStore.createTask(taskTitle, taskDescription, this.userStore.user.id)
             this.taskTitle = ""
             this.taskDescription = ""
+            this.succesfullTask = true
+            setTimeout(() => { this.succesfullTask = false }, 3500);
+
             /*  alert(this.succesfull) */
         },
         modalHandler(val) {
